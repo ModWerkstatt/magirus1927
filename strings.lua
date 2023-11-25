5288d4ac95afd54aa3947aae0fdfb8eabbfd5d7c
Florian Thiel <flth@flth.de>
2023-11-26 00:01:17 +0100

    initial

---
+++ strings.lua
@@ -0,0 +1,30 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Magirus year 1927",
+		["mod_desc"] = "Magirus truck from 1927, available as a flatbed, tarpaulin or tank.",
+
+        ["magirus_menu"] = "Magirus year 1927",
+        ["magirus_pritsche_name"] = "Magirus year 1927 flat bed",
+		["magirus_plane_name"] = "Magirus year 1927 w/ tarp",
+		["magirus_stangen_name"] = "Magirus year 1927 w/o tarp",
+		["magirus_tank_name"] = "Magirus year 1927 tanke",
+		["magirus_desc"] = "Magirus truck from 1927 with different bodies.",
+
+	},
+	de = {
+
+		["mod_name"] = "Magirus Bj. 1927",
+		["mod_desc"] = "Magirus LKW aus dem Baujahr 1927, verfügbar als Pritsche, Plane oder Tank.",
+
+        ["magirus_menu"] = "Magirus Bj. 1927",
+        ["magirus_pritsche_name"] = "Magirus Bj. 1927 Pritsche",
+		["magirus_plane_name"] = "Magirus Bj. 1927 Plane",
+		["magirus_stangen_name"] = "Magirus Bj. 1927 ohne Plane",
+		["magirus_tank_name"] = "Magirus Bj. 1927 Tank",
+		["magirus_desc"] = "Magirus LKW aus dem Jahr 1927 mit verschiedenen Aufbauten.",
+
+	},
+}
+end
